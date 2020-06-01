7c4069d211e52dda5109200f4842ff233852c76e
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-06-01 14:21:33 +0300

    minor changes

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello Heroku!
+          Hello Heroku (frontend deployed) !
         </a>
       </header>
     </div>
